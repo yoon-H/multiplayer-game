b07f33a85b0fc6b53ab998012515d765469aad91
yoon-H <ucando0320@gmail.com>
2025-01-09 17:03:40 +0900

    feat : add buffer in socket

--- src/events/onConnection.js
+++ src/events/onConnection.js
@@ -5,6 +5,9 @@ import { onError } from './onError.js';
 export const onConnection = (socket) => {
   console.log('클라이언트가 연결되었습니다:', socket.remoteAddress, socket.remotePort);
 
+  // 소켓에 버퍼 속성을 추가해서 클라이언트마다 고유한 버퍼 할당하기
+  socket.buffer = Buffer.alloc(0);
+
   socket.on('data', onData(socket));
   socket.on('end', onEnd(socket));
   socket.on('error', onError(socket));
