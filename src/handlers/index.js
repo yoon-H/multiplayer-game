150544b408b6e0f3c295f8f5f9ea006b687b8bf8
yoon-H <ucando0320@gmail.com>
2025-01-10 14:41:33 +0900

    feat : add index.js in handlers directory

---
+++ src/handlers/index.js
@@ -0,0 +1,15 @@
+const handlers = {};
+
+export const getHandlerById = (handlerId) => {
+  if (!handlers[handlerId]) {
+    console.error(`핸들러를 찾을 수 없습니다: ID ${handlerId}`);
+  }
+  return handlers[handlerId].handler;
+};
+
+export const getProtoTypeNameByHandlerId = (handlerId) => {
+  if (!handlers[handlerId]) {
+    console.error(`프로토타입을 찾을 수 없습니다: ID ${handlerId}`);
+  }
+  return handlers[handlerId].handler;
+};
