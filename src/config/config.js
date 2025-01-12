3adf03afbf1176da2dbc8e08524e977e101126e9
yoon-H <ucando0320@gmail.com>
2025-01-12 18:14:05 +0900

    feat : add db

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,17 @@
-import { CLIENT_VERSION, PORT, HOST } from '../constants/env.js';
+import {
+  CLIENT_VERSION,
+  PORT,
+  HOST,
+  DB1_NAME,
+  DB1_USER,
+  DB1_PASSWORD,
+  DB1_HOST,
+  DB2_NAME,
+  DB2_USER,
+  DB2_PASSWORD,
+  DB2_HOST,
+  DB2_PORT,
+} from '../constants/env.js';
 import { PACKET_TYPE_LENGTH, TOTAL_LENGTH } from '../constants/header.js';
 
 export const config = {
@@ -12,5 +25,21 @@ export const config = {
   packet: {
     totalLength: TOTAL_LENGTH,
     typeLength: PACKET_TYPE_LENGTH,
+  },
+  databases: {
+    GAME_DB: {
+      name: DB1_NAME,
+      user: DB1_USER,
+      password: DB1_PASSWORD,
+      host: DB1_HOST,
+      port: DB1_PORT,
+    },
+    USER_DB: {
+      name: DB2_NAME,
+      user: DB2_USER,
+      password: DB2_PASSWORD,
+      host: DB2_HOST,
+      port: DB2_PORT,
+    },
   },
 };
--- src/init/index.js
+++ src/init/index.js
@@ -1,11 +1,14 @@
 // 서버 초기화 작업
 import { loadGameAssets } from './assets.js';
 import { loadProtos } from './loadProtos.js';
+import { testAllConnections } from '../utils/db/testConnection.js';
+import pools from '../db/database.js';
 
 const initServer = async () => {
   try {
     await loadGameAssets();
     await loadProtos();
+    await testAllConnections(pools);
     // 다음 작업
   } catch (e) {
     console.error(e);
